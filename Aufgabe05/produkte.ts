d490c4724226e2cc1513b6cba7015eed801d8212
annaalehmann <64208557+annaalehmann@users.noreply.github.com>
2020-05-29 15:59:39 +0200

    fertig

---
+++ Aufgabe05/produkte.ts
@@ -0,0 +1,308 @@
+//Bauplan für die Produkte
+interface Produkt {
+    bild: string;
+    name: string;
+    beschreibung: string;
+    preis: string;
+    lippenstift: boolean;
+    lipliner: boolean;
+}
+
+
+//Jedem Produkt eine Variable geben
+//Lippenstifte
+let brave: Produkt = {
+    bild: "mac_lipstick/mac_brave.png",
+    name: "Brave",
+    beschreibung: "Ein cremiger Lippenstift mit weichem Gefühl, mittlerer bis voller, aufbaubarer Deckkraft und Satin finish.",
+    preis: "21,50€",
+    lippenstift: true,
+    lipliner: false
+};
+
+let mehr: Produkt = {
+    bild: "mac_lipstick/mac_mehr.png",
+    name: "Mehr",
+    beschreibung: "Eine cremig, reichhaltige Formel mit starker Farbpigmentierung und Mattem, nicht-glänzendem Finish.",
+    preis: "21,50€",
+    lippenstift: true,
+    lipliner: false
+
+};
+
+let faux: Produkt = {
+    bild: "mac_lipstick/mac_faux.png",
+    name: "Faux",
+    beschreibung: "Ein cremiger Lippenstift mit weichem Gefühl, mittlerer bis voller, aufbaubarer Deckkraft und Satin finish.",
+    preis: "21,50€",
+    lippenstift: true,
+    lipliner: false
+   
+};
+
+let twig: Produkt = {
+    bild: "mac_lipstick/mac_twig.png",
+    name: "Twig",
+    beschreibung: "Ein cremiger Lippenstift mit weichem Gefühl, mittlerer bis voller, aufbaubarer Deckkraft und Satin finish.",
+    preis: "21,50€",
+    lippenstift: true,
+    lipliner: false
+   
+};
+
+
+let kindaSexy: Produkt = {
+    bild: "mac_lipstick/mac_kinda_sexy.png",
+    name: "Kinda Sexy",
+    beschreibung: "Eine cremig, reichhaltige Formel mit starker Farbpigmentierung und Mattem, nicht-glänzendem Finish.",
+    preis: "21,50€",
+    lippenstift: true,
+    lipliner: false
+   
+};
+
+let velvetTeddy: Produkt = {
+    bild: "mac_lipstick/mac_velvet_teddy.png",
+    name: "Velvet Teddy",
+    beschreibung: "Eine cremig, reichhaltige Formel mit starker Farbpigmentierung und Mattem, nicht-glänzendem Finish.",
+    preis: "21,50€",
+    lippenstift: true,
+    lipliner: false
+ 
+};
+
+let mocha: Produkt = {
+    bild: "mac_lipstick/mac_mocha.png",
+    name: "Mocha",
+    beschreibung: "Ein cremiger Lippenstift mit weichem Gefühl, mittlerer bis voller, aufbaubarer Deckkraft und Satin finish.",
+    preis: "21,50€",
+    lippenstift: true,
+    lipliner: false
+ 
+};
+
+let whirl: Produkt = {
+    bild: "mac_lipstick/mac_whirl.png",
+    name: "Whirl",
+    beschreibung: "Eine cremig, reichhaltige Formel mit starker Farbpigmentierung und Mattem, nicht-glänzendem Finish.",
+    preis: "21,50€",
+    lippenstift: true,
+    lipliner: false
+   
+};
+
+let russianRed: Produkt = {
+    bild: "mac_lipstick/mac_russian_red.png",
+    name: "Russian Red",
+    beschreibung: "Eine cremig, reichhaltige Formel mit starker Farbpigmentierung und Mattem, nicht-glänzendem Finish.",
+    preis: "21,50€",
+    lippenstift: true,
+    lipliner: false
+  
+};
+
+let ladyDanger: Produkt = {
+    bild: "mac_lipstick/mac_lady_danger.png",
+    name: "Lady Danger",
+    beschreibung: "Eine cremig, reichhaltige Formel mit starker Farbpigmentierung und Mattem, nicht-glänzendem Finish.",
+    preis: "21,50€",
+    lippenstift: true,
+    lipliner: false
+  
+};
+
+let rubyWoo: Produkt = {
+    bild: "mac_lipstick/mac_ruby_woo.png",
+    name: "Ruby Woo",
+    beschreibung: "Eine cremig, reichhaltige Formel mit starker Farbpigmentierung und Mattem, nicht-glänzendem Finish.",
+    preis: "21,50€",
+    lippenstift: true,
+    lipliner: false
+};
+
+let fudge: Produkt = {
+    bild: "mac_lipstick/mac_double_fudge.png",
+    name: "Fudge",
+    beschreibung: "Eine cremig, reichhaltige Formel mit starker Farbpigmentierung und Mattem, nicht-glänzendem Finish.",
+    preis: "21,50€",
+    lippenstift: true,
+    lipliner: false
+};
+
+//Lipliner
+
+let cherryRed: Produkt = {
+    bild: "mac_lip_pensil/mac_cherry_red.png",
+    name: "Cherry Red",
+    beschreibung: "Lippenkonturenstift in einer großen Auswahl an Farbnuancen für das definieren, umranden oder ausfüllen der Lippen.",
+    preis: "17,50€",
+    lippenstift: false,
+    lipliner: true
+};
+
+let chicory: Produkt = {
+    bild: "mac_lip_pensil/mac_chicory.png",
+    name: "Chicory",
+    beschreibung: "Lippenkonturenstift in einer großen Auswahl an Farbnuancen für das definieren, umranden oder ausfüllen der Lippen.",
+    preis: "17,50€",
+    lippenstift: false,
+    lipliner: true
+};
+
+let cork: Produkt = {
+    bild: "mac_lip_pensil/mac_cork.png",
+    name: "Cork",
+    beschreibung: "Lippenkonturenstift in einer großen Auswahl an Farbnuancen für das definieren, umranden oder ausfüllen der Lippen.",
+    preis: "17,50€",
+    lippenstift: false,
+    lipliner: true
+};
+
+let dervish: Produkt = {
+    bild: "mac_lip_pensil/mac_dervish.png",
+    name: "Dervish",
+    beschreibung: "Lippenkonturenstift in einer großen Auswahl an Farbnuancen für das definieren, umranden oder ausfüllen der Lippen.",
+    preis: "17,50€",
+    lippenstift: false,
+    lipliner: true
+};
+
+let halfRed: Produkt = {
+    bild: "mac_lip_pensil/mac_half_red.png",
+    name: "Half Red",
+    beschreibung: "Lippenkonturenstift in einer großen Auswahl an Farbnuancen für das definieren, umranden oder ausfüllen der Lippen.",
+    preis: "17,50€",
+    lippenstift: false,
+    lipliner: true
+};
+
+let hover: Produkt = {
+    bild: "mac_lip_pensil/mac_hover.png",
+    name: "Hover",
+    beschreibung: "Lippenkonturenstift in einer großen Auswahl an Farbnuancen für das definieren, umranden oder ausfüllen der Lippen.",
+    preis: "17,50€",
+    lippenstift: false,
+    lipliner: true
+};
+
+let oak: Produkt = {
+    bild: "mac_lip_pensil/mac_oak.png",
+    name: "Oak",
+    beschreibung: "Lippenkonturenstift in einer großen Auswahl an Farbnuancen für das definieren, umranden oder ausfüllen der Lippen.",
+    preis: "17,50€",
+    lippenstift: false,
+    lipliner: true
+};
+
+let macRedd: Produkt = {
+    bild: "mac_lip_pensil/mac_redd.png",
+    name: "Mac Redd",
+    beschreibung: "Lippenkonturenstift in einer großen Auswahl an Farbnuancen für das definieren, umranden oder ausfüllen der Lippen.",
+    preis: "17,50€",
+    lippenstift: false,
+    lipliner: true
+};
+
+let rosyRim: Produkt = {
+    bild: "mac_lip_pensil/mac_rosy_rim.png",
+    name: "Rosy Rim",
+    beschreibung: "Lippenkonturenstift in einer großen Auswahl an Farbnuancen für das definieren, umranden oder ausfüllen der Lippen.",
+    preis: "17,50€",
+    lippenstift: false,
+    lipliner: true
+};
+
+let rubyWoo2: Produkt = {
+    bild: "mac_lip_pensil/mac_ruby_woo.png",
+    name: "Ruby Woo",
+    beschreibung: "Lippenkonturenstift in einer großen Auswahl an Farbnuancen für das definieren, umranden oder ausfüllen der Lippen.",
+    preis: "17,50€",
+    lippenstift: false,
+    lipliner: true
+};
+
+let sensation: Produkt = {
+    bild: "mac_lip_pensil/mac_sensation.png",
+    name: "Sensation",
+    beschreibung: "Lippenkonturenstift in einer großen Auswahl an Farbnuancen für das definieren, umranden oder ausfüllen der Lippen.",
+    preis: "17,50€",
+    lippenstift: false,
+    lipliner: true
+};
+
+let soar: Produkt = {
+    bild: "mac_lip_pensil/mac_soar.png",
+    name: "Soar",
+    beschreibung: "Lippenkonturenstift in einer großen Auswahl an Farbnuancen für das definieren, umranden oder ausfüllen der Lippen.",
+    preis: "17,50€",
+    lippenstift: false,
+    lipliner: true
+};
+
+//Auf HTML Elemente zugreifen 
+
+let array: Array<Produkt> = [brave, mehr, faux, twig, kindaSexy, velvetTeddy, mocha, whirl, russianRed, ladyDanger, rubyWoo, fudge, cherryRed,
+                            chicory, cork, dervish, halfRed, hover, oak, macRedd, rosyRim, rubyWoo2, sensation, soar];
+
+
+for (let i: number = 0; i < array.length; i++) {
+
+    if (array[i].lippenstift) {
+
+        let  div: HTMLDivElement = document.createElement("div");
+        div.id = "produktLippenstift" + i;
+        document.getElementById("Lippenstift")?.appendChild(div);
+       
+        let bildLippenstift: HTMLImageElement = document.createElement("img");
+        bildLippenstift.src = array[i].bild;
+        document.getElementById("produktLippenstift" + i)?.appendChild(bildLippenstift);
+       
+        let nameLippenstift: HTMLElement = document.createElement("h3");
+        nameLippenstift.innerHTML = array[i].name;
+        document.getElementById("produkLippenstift" + i)?.appendChild(nameLippenstift);
+ 
+        let beschreibungLippenstift: HTMLElement = document.createElement("p");
+        beschreibungLippenstift.innerHTML = array[i].beschreibung;
+        document.getElementById("produktLippenstift" + i)?.appendChild(beschreibungLippenstift);
+   
+        let preisLippenstift: HTMLElement = document.createElement("h4");
+        preisLippenstift.innerHTML = array[i].preis;
+        document.getElementById("produktLippenstift" + i)?.appendChild(preisLippenstift);
+        
+        
+        let button: HTMLElement = document.createElement("button");
+        document.getElementById(array[i].name)?.appendChild(button);
+        document.getElementById("produktLippenstift" + i)?.appendChild(button);
+        button.innerHTML = "In den Warenkorb";
+    }
+
+    else if (array[i].lipliner){
+    
+    let  div: HTMLDivElement = document.createElement("div");
+    div.id = "produktLipliner" + i;
+    document.getElementById("Lipliner")?.appendChild(div);
+   
+    let bildLipliner: HTMLImageElement = document.createElement("img");
+    bildLipliner.src = array[i].bild;
+    document.getElementById("produktLipliner" + i)?.appendChild(bildLipliner);
+   
+    let nameLipliner: HTMLElement = document.createElement("h3");
+    nameLipliner.innerHTML = array[i].name;
+    document.getElementById("produktLipliner" + i)?.appendChild(nameLipliner);
+
+    let beschreibungLipliner: HTMLElement = document.createElement("p");
+    beschreibungLipliner.innerHTML = array[i].beschreibung;
+    document.getElementById("produktLipliner" + i)?.appendChild(beschreibungLipliner);
+
+    let preisLipliner: HTMLElement = document.createElement("h4");
+    preisLipliner.innerHTML = array[i].preis;
+    document.getElementById("produktLipliner" + i)?.appendChild(preisLipliner);
+    
+    
+    let button: HTMLElement = document.createElement("button");
+    document.getElementById(array[i].name)?.appendChild(button);
+    document.getElementById("produktLipliner" + i)?.appendChild(button);
+    button.innerHTML = "In den Warenkorb";
+
+}
+}
