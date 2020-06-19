d98bc08cceb9662a20ab8b152640d8f81633042b
annaalehmann <64208557+annaalehmann@users.noreply.github.com>
2020-06-20 01:02:44 +0200

    8 fertig

--- Aufgabe08/script.js
+++ Aufgabe08/script.js
@@ -10,7 +10,7 @@ var Aufgabe082;
     }
     function clickButton() {
         let formData = new FormData(document.forms[0]);
-        let url = "https://annaalehmanngis.herokuapp.com";
+        let url = "https://annaalehmanngis.herokuapp.com/";
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
         laden(url);
