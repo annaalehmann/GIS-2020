53e8130da32c4fcf5a60ee62a36b0a71eebf88c5
annaalehmann <64208557+annaalehmann@users.noreply.github.com>
2020-06-21 20:57:31 +0200

    asdfgh

--- Aufgabe07/script.ts
+++ Aufgabe07/script.ts
@@ -1,5 +1,15 @@
 namespace Aufgabe07 {
 
+    loadArtikel("produkte.json");
+
+    async function loadArtikel(_url: RequestInfo): Promise<void> {
+        let response: Response = await fetch(_url);
+        let jsonArray: JSON = await response.json();
+        console.log(jsonArray);
+        produkte = await JSON.parse(JSON.stringify(jsonArray));
+        console.log(produkte);
+        get();
+}
 
     export function get(): void {
         for (let i: number = 0; i < produkt.length; i++) {
